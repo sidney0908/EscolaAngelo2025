8e2d688534b4c637a682fc0ce12eeef95aee89ad
SIDNEY JUNIOR <00001124896995sp@al.educacao.sp.gov.br>
2025-03-20 07:29:12 -0300

    Add files via upload

---
+++ aula22.py
@@ -0,0 +1,32 @@
+# EXERCÍCIOS WHILE
+
+#  1. Adivinhação de números:
+#  • Criem uma lista com 10 números.
+#  • Peçam ao usuário para adivinhar um número da lista.
+#  • Usem um loop while para continuar pedindo adivinhações até que o usuário acerte.
+
+#  2. Contagem regressiva:
+#  • Criem uma lista de contagem regressiva de 10 a 1.
+#  • Usem um loop while para imprimir cada número da lista
+
+# 3. Adição de números:
+#  • Criem uma lista vazia para armazenar números.
+#  • Peçam ao usuário para fornecer números e os adicionem à lista.
+#  • Continuem pedindo números até que o usuário decida parar.
+
+#  4.Média de notas:
+#  • Criem uma lista vazia para armazenar notas.
+#  • Peçam ao usuário para fornecer notas e as adicionem à lista.
+#  • Calculem e imprimam a média das notas quando o usuário decidir parar
+
+# 5.Busca em lista:
+#  • Criem uma lista de cinco nomes.
+#  • Peçam ao usuário para digitar um nome.
+#  • Usem um loop while para verificar se o nome está na 
+# lista e informar o resultado.
+
+#  6. Contador de números:
+#  • Solicitem ao usuário um número inicial.
+#  • Usem um loop while para imprimir os números de 1 até o 
+# número fornecido pelo usuário.
+#  • Exibam uma mensagem indicando que o loop terminou
